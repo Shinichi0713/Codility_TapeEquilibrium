c02a05f1909c5a1efe7ae6cd254ef1a5c4900a77
Shinichi Yoshinaga <61480734+Shinichi0713@users.noreply.github.com>
2024-05-25 18:01:09 +0900

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,17 @@
+def solution(A):
+    total_sum = sum(A)
+    left_sum = 0
+    min_diff = float('inf')
+
+    for i in range(len(A) - 1):
+        left_sum += A[i]
+        right_sum = total_sum - left_sum
+        diff = abs(left_sum - right_sum)
+
+        if diff < min_diff:
+            min_diff = diff
+
+    return min_diff
+
+
+print(solution([-1000, 1000]))
